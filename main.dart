b7e7e4a002d790c7b56066cc59719133c56c8a17
fidelite250 <mfidelite7@gmail.com>
2026-02-22 22:34:06 +0200

    add main.dart to async programming practices

--- main.dart
+++ main.dart
@@ -1,26 +1,21 @@
-main() async {
+void main() async {
   print("Starting Async Block");
 
   try {
-    Future<String> future_data = fectchData();
-    future_data.then((value) {
-      print(value);
-
-      ///Do other things
-    });
-    print("Ending Synchronous Block");
+    String futureData = await fetchData();
+    print(futureData);
   } catch (e) {
-    
+    print("Error occurred: $e");
   }
+
+  print("Ending Synchronous Block");
 }
 
-Future<String> fectchData() {
-  String data = "No Data";
+Future<String> fetchData() {
   print("Fetching Data...");
 
-  Duration duration = Duration(seconds: 3);
-
-  return Future.delayed(duration, () {
-    return "All Data Here";
-  });
+  return Future.delayed(
+    Duration(seconds: 3),
+    () => "All Data Here",
+  );
 }
